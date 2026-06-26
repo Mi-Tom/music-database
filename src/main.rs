9df05bfa2f3276f8c752b8b058499fbbec0d3352
M. Tomanec <mtomanec7@gmail.com>
2026-06-26 18:34:32 +0200

    vyrvorena struktura - use uuid

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use uuid::Uuid;
+
+struct Song {
+    uuid: Uuid,
+    title: String,
+    artists: String,
+    album: Option<String>,
+    duration: Option<i32>,
+    year: Option<i32>
+}
+
 fn main() {
     println!("hi sky");
 }
