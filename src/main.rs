1638e56de65d5e04a5e8b9b4493a3b91183bc0bd
M. Tomanec <mtomanec7@gmail.com>
2026-06-27 20:01:09 +0200

    vytvoreny metody pro pridavani a vypis + napsany testy

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-use uuid::Uuid;
-
-struct Song {
-    uuid: Uuid,
-    title: String,
-    artists: String,
-    album: Option<String>,
-    duration: Option<i32>,
-    year: Option<i32>
-}
-
 fn main() {
     println!("hi sky");
 }
